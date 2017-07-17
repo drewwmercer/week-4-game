594022eb2651ebacbd113b7aeda4b3bd58e8a247
Andrew Mercer <japanexpress@gmail.com>
2017-07-17 11:44:56 -0400

    collecting random values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,26 @@
-  // Variables
-    var randomNumber;
-    var crystalOneValue;
-    var crystalTwoValue;
-    var crystalThreeValue;
-    var crystalFourValue;
-    var winCounter;
-    var lossCounter;
-    var currentTally;
+// Variables
+var randomNumber;
+var crystalOneValue;
+var crystalTwoValue;
+var crystalThreeValue;
+var crystalFourValue;
+var winCounter;
+var lossCounter;
+var currentTally;
 
-  $(document).ready(function () {
+$(document).ready(function() {
+  $("#random-button").on("click", function() {
+    var randomNumber = Math.floor(Math.random() * (120 - 19) + 19);
+    $("#random-number").html(randomNumber);
+    var crystalOneValue = Math.floor(Math.random() * (12 - 1) + 1);
+    var crystalTwoValue = Math.floor(Math.random() * (12 - 1) + 1);
+    var crystalThreeValue = Math.floor(Math.random() * (12 - 1) + 1);
+    var crystalFourValue = Math.floor(Math.random() * (12 - 1) + 1);
 
-$("#random-button").on("click", function () {
-        var randomNumber = (Math.floor(Math.random() * (120 - 19) +
-            19));
-        $("#random-number").html(randomNumber);
-    });
-
-
+    console.log("random: " + randomNumber);
+    console.log("one: " + crystalOneValue);
+    console.log("two: " + crystalTwoValue);
+    console.log("three: " + crystalThreeValue);
+    console.log("four: " + crystalFourValue);
+  });
 });
