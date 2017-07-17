a712ae2315ce1f505657137c98e65c6df05ccbb2
Andrew Mercer <japanexpress@gmail.com>
2017-07-17 12:35:10 -0400

    making images clickable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,15 @@ var crystalOneValue;
 var crystalTwoValue;
 var crystalThreeValue;
 var crystalFourValue;
-var winCounter;
-var lossCounter;
-var currentTally;
+var winCounter = 0;
+var lossCounter = 0;
+var currentCount = 0;
 
 $(document).ready(function() {
+  $("#win-count").html("Wins: " + winCounter)
+  $("#loss-count").html("Losses: " + lossCounter)
+  $
+  
   $("#random-button").on("click", function() {
     var randomNumber = Math.floor(Math.random() * (120 - 19) + 19);
     $("#random-number").html(randomNumber);
@@ -22,5 +26,9 @@ $(document).ready(function() {
     console.log("two: " + crystalTwoValue);
     console.log("three: " + crystalThreeValue);
     console.log("four: " + crystalFourValue);
+  });
+
+  $(".blue-gem").on("click", function() {
+    $("#current-count").html(currentCount + crystalOneValue)
   });
 });
